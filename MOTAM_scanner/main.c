a839e58730927e5f4f2f350df45a9775b1493d33
Manuel Montenegro <manumonti@gmail.com>
2018-09-07 14:17:17 +0200

    Updated to SDK v15.1.0 and SoftDevice v6.1.0

--- MOTAM_scanner/main.c
+++ MOTAM_scanner/main.c
@@ -1,158 +1,74 @@
 /***************************************************************************************/
 /*
- * MOTAM_scanner
- * Created by Manuel Montenegro, Jun 13, 2018.
+ * motam_scanner
+ * Created by Manuel Montenegro, Sep 7, 2018.
  * Developed for MOTAM project.
  *
  *  This is a Bluetooth 5 scanner. This code reads every advertisement from MOTAM beacons
- *  and send its data through serial port.
+ *  and sends its data through serial port.
  *
  *  This code has been developed for Nordic Semiconductor nRF52840 PDK.
 */
 /***************************************************************************************/
 
-#include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 #include "nordic_common.h"
-#include "app_error.h"
-#include "app_uart.h"
-#include "app_timer.h"
-#include "app_util.h"
-#include "bsp_btn_ble.h"
+#include "nrf_sdm.h"
 #include "ble.h"
-#include "ble_gap.h"
 #include "ble_hci.h"
+#include "ble_db_discovery.h"
+#include "ble_srv_common.h"
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
 #include "nrf_sdh_soc.h"
-#include "ble_advdata.h"
 #include "nrf_pwr_mgmt.h"
+#include "app_util.h"
+#include "app_error.h"
+#include "ble_dis_c.h"
+#include "ble_rscs_c.h"
+#include "app_util.h"
+#include "app_timer.h"
+#include "bsp_btn_ble.h"
+#include "peer_manager.h"
+#include "peer_manager_handler.h"
+#include "fds.h"
+#include "nrf_fstorage.h"
+#include "ble_conn_state.h"
+#include "nrf_ble_gatt.h"
+#include "nrf_pwr_mgmt.h"
+#include "nrf_ble_scan.h"
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
 
-#define APP_BLE_CONN_CFG_TAG    1                                       /**< A tag that refers to the BLE stack configuration we set with @ref sd_ble_cfg_set. Default tag is @ref BLE_CONN_CFG_TAG_DEFAULT. */
-#define APP_BLE_OBSERVER_PRIO   3                                       /**< Application's BLE observer priority. You shoulnd't need to modify this value. */
+#define APP_BLE_CONN_CFG_TAG        1                                   /**< Tag that identifies the BLE configuration of the SoftDevice. */
+#define APP_BLE_OBSERVER_PRIO       3                                   /**< BLE observer priority of the application. There is no need to modify this value. */
+#define APP_SOC_OBSERVER_PRIO       1                                   /**< SoC observer priority of the application. There is no need to modify this value. */
 
-#define UART_TX_BUF_SIZE        256                                     /**< UART TX buffer size. */
-#define UART_RX_BUF_SIZE        256                                     /**< UART RX buffer size. */
+#define SCAN_INTERVAL               0x0320                              /**< Determines scan interval in units of 0.625 millisecond. */
+#define SCAN_WINDOW                 0x0320                              /**< Determines scan window in units of 0.625 millisecond. */
+#define SCAN_DURATION           	0x0000                              /**< Duration of the scanning in units of 10 milliseconds. If set to 0x0000, scanning continues until it is explicitly disabled. */
 
-#define SCAN_INTERVAL           0x0320                                  // [MOTAM] Determines scan interval in units of 0.625 millisecond -> 0x0320 = 500ms.*/
-#define SCAN_WINDOW             0x0320                                  // [MOTAM] Determines scan window in units of 0.625 millisecond -> 0x0320 = 500ms.*/
-#define SCAN_DURATION           0x0000                                  /**< Timout when scanning. 0x0000 disables timeout. */
+NRF_BLE_SCAN_DEF(m_scan);                                   /**< Scanning Module instance. */
 
+static bool                  m_memory_access_in_progress;   /**< Flag to keep track of ongoing operations on persistent memory. */
 
-static uint8_t m_scan_buffer_data[BLE_GAP_SCAN_BUFFER_EXTENDED_MIN];	// [MOTAM] Buffer for extended advertisements
-//static uint8_t m_scan_buffer_data[BLE_GAP_SCAN_BUFFER_MIN]; 			// [MOTAM] Buffer for non extended advertisements
-
-/**@brief Pointer to the buffer where advertising reports will be stored by the SoftDevice. */
-static ble_data_t m_scan_buffer =
+static ble_gap_scan_params_t m_scan_param =                 /**< Scan parameters requested for scanning and connection. */
 {
-    m_scan_buffer_data,
-//    BLE_GAP_SCAN_BUFFER_MIN			// [MOTAM] Buffer size for non extended advertisements
-	BLE_GAP_SCAN_BUFFER_EXTENDED_MIN	// [MOTAM] Buffer size for extended advertisements
+    .active        = 0x00,
+    .interval      = SCAN_INTERVAL,
+    .window        = SCAN_WINDOW,
+    .filter_policy = BLE_GAP_SCAN_FP_ACCEPT_ALL,
+    .timeout       = SCAN_DURATION,
+    .scan_phys     = BLE_GAP_PHY_CODED,
+    .extended      = 1,
 };
 
-/** @brief Parameters used when scanning. */
-static ble_gap_scan_params_t const m_scan_params =
-{
-	.active   = 0,
-	.extended = 1,
-    .interval = SCAN_INTERVAL,
-    .window   = SCAN_WINDOW,
-    .timeout  = SCAN_DURATION,
-
-	// [MOTAM] Choose only one of the following scan_phys
-//	.scan_phys        = BLE_GAP_PHY_1MBPS,				// [MOTAM] Scan 1MBPS advertisements
-	.scan_phys        = BLE_GAP_PHY_CODED,				// [MOTAM] Scan PHY_CODED advertisements
-	
-	.filter_policy    = BLE_GAP_SCAN_FP_ACCEPT_ALL,
-};
-
-
-/**@brief Function for asserts in the SoftDevice.
- *
- * @details This function will be called in case of an assert in the SoftDevice.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyze
- *          how your product is supposed to react in case of Assert.
- * @warning On assert from the SoftDevice, the system can only recover on reset.
- *
- * @param[in] line_num     Line number of the failing ASSERT call.
- * @param[in] p_file_name  File name of the failing ASSERT call.
- */
-void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
-{
-    app_error_handler(0xDEADBEEF, line_num, p_file_name);
-}
-
-
-/**@brief Function to start scanning. */
-static void scan_start(void)
-{
-    ret_code_t ret;
-
-    ret = sd_ble_gap_scan_start(&m_scan_params, &m_scan_buffer);
-    APP_ERROR_CHECK(ret);
-
-    ret = bsp_indication_set(BSP_INDICATE_SCANNING);
-    APP_ERROR_CHECK(ret);
-}
-
-
-
-/**@brief   Function for handling app_uart events.
- *
- * @details This function will receive a single character from the app_uart module and append it to
- *          a string. The string will be be sent over BLE when the last character received was a
- *          'new line' '\n' (hex 0x0A) or if the string has reached the maximum data length.
- */
-void uart_event_handle(app_uart_evt_t * p_event)
-{
-    switch (p_event->evt_type)
-    {
-        /**@snippet [Handling data from UART] */
-        case APP_UART_COMMUNICATION_ERROR:
-            NRF_LOG_ERROR("Communication error occurred while handling UART.");
-            APP_ERROR_HANDLER(p_event->data.error_communication);
-            break;
-
-        case APP_UART_FIFO_ERROR:
-            NRF_LOG_ERROR("Error occurred in FIFO module used by UART.");
-            APP_ERROR_HANDLER(p_event->data.error_code);
-            break;
-
-        default:
-            break;
-    }
-}
-
-
-
-/**
- * [MOTAM] Function for handling the advertising report BLE event.
- *	This function filters every advertisements and prints MOTAM beacons through serial port.
- */
-static void on_adv_report(ble_gap_evt_adv_report_t const * p_adv_report)
-{
-    ret_code_t err_code;
-
-	if ((p_adv_report->data.p_data[2] == 0xBE) && (p_adv_report->data.p_data[3] == 0xDE))
-	{
-		for (uint8_t i = 0; i < p_adv_report->data.len; i++) {
-			printf ("%02x", p_adv_report->data.p_data[i]);
-		}
-		printf("\r\n");
-		fflush(stdout);
-	}
-
-	err_code = sd_ble_gap_scan_start(NULL, &m_scan_buffer);
-	APP_ERROR_CHECK(err_code);
-
-}
+static void scan_start(void);
 
 
 /**@brief Function for handling BLE events.
@@ -162,15 +78,44 @@ static void on_adv_report(ble_gap_evt_adv_report_t const * p_adv_report)
  */
 static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
 {
-    ble_gap_evt_t const * p_gap_evt = &p_ble_evt->evt.gap_evt;
 
     switch (p_ble_evt->header.evt_id)
     {
         case BLE_GAP_EVT_ADV_REPORT:
-            on_adv_report(&p_gap_evt->params.adv_report);
-            break; // BLE_GAP_EVT_ADV_REPORT
+        {
+            NRF_LOG_RAW_HEXDUMP_INFO (m_scan.scan_buffer.p_data, m_scan.scan_buffer.len);
+            NRF_LOG_RAW_INFO ("----------------------------------\r\n");
+        }
 
         default:
+            break;
+    }
+}
+
+
+/**
+ * @brief SoftDevice SoC event handler.
+ *
+ * @param[in] evt_id    SoC event.
+ * @param[in] p_context Context.
+ */
+static void soc_evt_handler(uint32_t evt_id, void * p_context)
+{
+    switch (evt_id)
+    {
+        case NRF_EVT_FLASH_OPERATION_SUCCESS:
+        /* fall through */
+        case NRF_EVT_FLASH_OPERATION_ERROR:
+
+            if (m_memory_access_in_progress)
+            {
+                m_memory_access_in_progress = false;
+                scan_start();
+            }
+            break;
+
+        default:
+            // No implementation needed.
             break;
     }
 }
@@ -179,7 +124,7 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
 /**@brief Function for initializing the BLE stack.
  *
  * @details Initializes the SoftDevice and the BLE event interrupt.
- */
+  */
 static void ble_stack_init(void)
 {
     ret_code_t err_code;
@@ -197,36 +142,73 @@ static void ble_stack_init(void)
     err_code = nrf_sdh_ble_enable(&ram_start);
     APP_ERROR_CHECK(err_code);
 
-    // Register a handler for BLE events.
+    // Register handlers for BLE and SoC events.
     NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
+    NRF_SDH_SOC_OBSERVER(m_soc_observer, APP_SOC_OBSERVER_PRIO, soc_evt_handler, NULL);
 }
 
 
+/**@brief Function for handling Scanning Module events.
+ */
+static void scan_evt_handler(scan_evt_t const * p_scan_evt)
+{
+    switch(p_scan_evt->scan_evt_id)
+    {
+        case NRF_BLE_SCAN_EVT_SCAN_TIMEOUT:
+        {
+            NRF_LOG_INFO("Scan timed out.");
+            scan_start();
+        } break;
 
-/**@brief Function for initializing the UART. */
-static void uart_init(void)
+        default:
+          break;
+    }
+}
+
+
+/**@brief Function for initializing the scanning and setting the filters.
+ */
+static void scan_init(void)
+{
+    ret_code_t          err_code;
+    nrf_ble_scan_init_t init_scan;
+
+    memset(&init_scan, 0, sizeof(init_scan));
+
+    init_scan.connect_if_match = false;
+    init_scan.conn_cfg_tag     = APP_BLE_CONN_CFG_TAG;
+    init_scan.p_scan_param     = &m_scan_param;
+
+    err_code = nrf_ble_scan_init(&m_scan, &init_scan, scan_evt_handler);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for starting scanning.
+ */
+static void scan_start(void)
 {
     ret_code_t err_code;
 
-    app_uart_comm_params_t const comm_params =
+    // If there is any pending write to flash, defer scanning until it completes.
+    if (nrf_fstorage_is_busy(NULL))
     {
-        .rx_pin_no    = RX_PIN_NUMBER,
-        .tx_pin_no    = TX_PIN_NUMBER,
-        .rts_pin_no   = RTS_PIN_NUMBER,
-        .cts_pin_no   = CTS_PIN_NUMBER,
-        .flow_control = APP_UART_FLOW_CONTROL_DISABLED,
-        .use_parity   = false,
-        .baud_rate    = UART_BAUDRATE_BAUDRATE_Baud115200
-    };
+        m_memory_access_in_progress = true;
+        return;
+    }
 
-    APP_UART_FIFO_INIT(&comm_params,
-                       UART_RX_BUF_SIZE,
-                       UART_TX_BUF_SIZE,
-                       uart_event_handle,
-                       APP_IRQ_PRIORITY_LOWEST,
-                       err_code);
-
+    err_code = nrf_ble_scan_start(&m_scan);
     APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for initializing logging. */
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
 
@@ -235,16 +217,6 @@ static void timer_init(void)
 {
     ret_code_t err_code = app_timer_init();
     APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for initializing the nrf log module. */
-static void log_init(void)
-{
-    ret_code_t err_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(err_code);
-
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
 
@@ -258,10 +230,9 @@ static void power_management_init(void)
 }
 
 
-
 /**@brief Function for handling the idle state (main loop).
  *
- * @details Handle any pending log operation(s), then sleep until the next event occurs.
+ * @details Handles any pending log operations, then sleeps until the next event occurs.
  */
 static void idle_state_handle(void)
 {
@@ -272,19 +243,23 @@ static void idle_state_handle(void)
 }
 
 
+
 int main(void)
 {
+
     // Initialize.
     log_init();
     timer_init();
-    uart_init();
     power_management_init();
     ble_stack_init();
+    scan_init();
 
-//    // Start execution.
-    printf("MOTAM SCANNER.\r\n");
-    fflush(stdout);
+    // Start execution.
+    NRF_LOG_RAW_INFO("  -------------\r\n");
+    NRF_LOG_RAW_INFO(	 "| MOTAM scanner |");
+    NRF_LOG_RAW_INFO("\r\n  -------------\r\n");
     scan_start();
+
 
     // Enter main loop.
     for (;;)
--- README.md
+++ README.md
@@ -6,18 +6,18 @@ This application captures advertisements with extended advertising characteristi
 
 The application sends the captured advertisements through serial port.
 
-This repository is part of [MOTAM project](https://www.nics.uma.es/projects/motam), but you can use Beacon_Scanner for general purposes.
+This repository is part of [MOTAM project](https://www.nics.uma.es/projects/motam), but you can use beacon_scanner for general purposes.
 
 ## Requeriments
 
 Currently, this project uses:
--   nRF5 SDK version 15.0.0
--   S140 SoftDevice v6.0.0 API
--   nRF52840 DK (PCA10056)
+-   nRF5 SDK version 15.1.0
+-   S140 SoftDevice v6.1.0 API
+-   nRF52840 PDK (PCA10056)
 
 ## Get started
 
-To compile it, put the folders inside nRF52 into the \nRF5_SDK_15.0.0\examples\ble_central folder.
+To compile it, put the folders inside nRF52 into the \nRF5_SDK_15.1.0\examples\ble_central folder.
 
 In order to stablish serial connection with your PC, you can use PUTTY if you are a Windows user or GNU Screen if you are a Linux user.
 
@@ -37,6 +37,6 @@ Where */dev/ttyACM0* is the nRF52840 PDK device. You can know what is the path o
 
 ## Applications' description
 
-- **Beacon_scanner:** This application is for general purposes. It simply reads each beacon advertisements and prints it by serial port. Currently this only reads PHY Coded advertisements (long range advertisements), but you can easily change the used PHY from code.
+- **beacon_scanner:** This application is for general purposes. It simply reads each beacon advertisements and prints it by serial port. Currently this only reads PHY Coded advertisements (long range advertisements), but you can easily change the used PHY from code.
 
 - **MOTAM_scanner:** Reads advertisements and filters it in order to show by serial port only the MOTAM beacons. In this case, the nRF52840 will be connected by USB port to the [MOTAM gateway](https://github.com/nicslabdev/MOTAM-Gateway). MOTAM beacons report on the state of the environment, so the gateway will collect this information.
